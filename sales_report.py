bf77aa293aacf93378f5d51ba673ce8857fae5b6
Emily Wagner <emily.wagner@gmail.com>
2014-06-08 09:15:02 -0700

    Added comments to make the script better/faster

--- sales_report.py
+++ sales_report.py
@@ -3,6 +3,17 @@ sales_report.py - Generates sales report showing the total number
                   of melons each sales person sold.
 
 """
+# Suggested Improvements
+# 1. Create some functions, like generate_sales_report() instead of
+#    all of the code in main.  This will make it easier to read, reuse, and organize.
+# 2. Use tuple assignment when parsing each line of the csv.  Tuple assignment is concise and cool.
+# 3. Use a dictionary instead of processing the data as two lists. This will make the script faster
+#    when the list of salespeople is very long.
+# 4. Make the report file a command line argument that you feed in when running the script vs. 
+#    hard-coding.  This will make the script more flexible.
+# 5. The dollar total for each sales order seems important.  Maybe we should also report
+#    the amount of money each salesperson is generating, too.  And/or also the 
+#    (order total/number of melons) for a measure of "sales efficiency".
 
 def main():
     salespeople = []
